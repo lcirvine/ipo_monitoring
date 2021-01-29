243fdf11752d5df06af757fb555534523c533ced
lcirvine <55790560+lcirvine@users.noreply.github.com>
2021-01-29 14:57:55 +0000

    Adding a test function to file management

--- file_management.py
+++ file_management.py
@@ -25,13 +25,35 @@ def delete_old_files(folder: str, num_days: int = 30) -> list:
     return files_deleted
 
 
+def delete_old_files_test(folder: str, num_days: int = 30) -> list:
+    """
+    Used to test the delete_old_files function. This will only print the name of the files that would be deleted.
+    :param folder: folder location files will be deleted from
+    :param num_days: int specifying the number of days before a file is deleted
+    :return: list of files that were deleted
+    """
+    old_date = datetime.utcnow() - timedelta(days=num_days)
+    files_deleted = []
+    for root, dirs, files in os.walk(folder):
+        for file in files:
+            f_abs = os.path.join(root, file)
+            f_modified = datetime.fromtimestamp(os.path.getmtime(f_abs))
+            if f_modified <= old_date:
+                print(f_abs)
+                files_deleted.append(file)
+    if len(files_deleted) > 0:
+        print(f"Deleted {files_deleted}")
+    return files_deleted
+
+
 def main():
-    try:
-        delete_old_files(os.path.join(os.getcwd(), 'Results'))
-    except Exception as e:
-        logger.error(e, exc_info=sys.exc_info())
-        error_email(str(e))
-        logger.info('-' * 100)
+    for folder in [os.path.join(os.getcwd(), f) for f in ['Results', 'Logs']]:
+        try:
+            delete_old_files(folder)
+        except Exception as e:
+            logger.error(e, exc_info=sys.exc_info())
+            error_email(str(e))
+            logger.info('-' * 100)
 
 
 if __name__ == '__main__':
