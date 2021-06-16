3935fa58fa77ac9bbee9a54557f31a2c66557843
lcirvine <55790560+lcirvine@users.noreply.github.com>
2021-06-16 11:39:10 +0100

    Calling transformation for db tables

--- ipo_monitoring.py
+++ ipo_monitoring.py
@@ -2,6 +2,7 @@ from logging_ipo_dates import logger, consolidate_webscraping_results
 import source_reference
 import website_scraping
 import data_transformation
+import data_transformation_db
 import entity_mapping
 import data_comparison
 import file_management
@@ -11,6 +12,7 @@ logger.info('-' * 100)
 source_reference.main()
 website_scraping.main()
 data_transformation.main()
+data_transformation_db.main()
 entity_mapping.main()
 df_summary = data_comparison.main()
 # Note: email_report is being called separately with another batch file because the run schedule is different
